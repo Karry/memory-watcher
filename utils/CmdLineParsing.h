a5b978b6b518d6fd238034d9936398625426b67a
Lukáš Karas <lukas.karas@avast.com>
2021-10-08 09:35:20 +0200

    add CmdLineParsing from libosmscout project

---
+++ utils/CmdLineParsing.h
@@ -0,0 +1,405 @@
+#ifndef CMDLINEPARSING_H
+#define CMDLINEPARSING_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2017  Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+
+#include "String.h"
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include <list>
+
+class CmdLineScanner
+{
+private:
+  std::vector<std::string> arguments;
+  size_t                   nextArg;
+
+public:
+  CmdLineScanner(int argc, char* argv[]); // NOLINT
+
+  explicit CmdLineScanner(const std::vector<std::string>& arguments);
+
+  bool HasNextArg() const;
+  std::string PeakNextArg() const;
+  std::string Advance();
+  std::string GetCurrentArg() const;
+};
+
+
+class CmdLineParseResult
+{
+private:
+  bool        hasError;
+  std::string errorDescription;
+
+public:
+  CmdLineParseResult();
+
+  explicit CmdLineParseResult(const std::string& errorDescription);
+
+  bool Success() const;
+  bool HasError() const;
+
+  std::string GetErrorDescription() const;
+};
+
+class CmdLineArgParser
+{
+private:
+  std::string optionName;   //<! The unqualified option name
+  std::string argumentName; //<! The actual command line argument name
+
+protected:
+  std::string GetOptionName() const;
+  std::string GetArgumentName() const;
+
+public:
+  virtual ~CmdLineArgParser()=default;
+
+  void SetOptionName(const std::string& optionName);
+  void SetArgumentName(const std::string& argumentName);
+
+  virtual std::string GetOptionHint() const = 0;
+  virtual std::string GetPositionalHint(const std::string& positional) const = 0;
+  virtual CmdLineParseResult Parse(CmdLineScanner& scanner) = 0;
+};
+
+using CmdLineArgParserRef = std::shared_ptr<CmdLineArgParser>;
+
+class CmdLineFlagArgParser : public CmdLineArgParser
+{
+public:
+  using SetterFunction = std::function<void (const bool &)>;
+
+private:
+   SetterFunction setter;
+
+public:
+  explicit CmdLineFlagArgParser(SetterFunction&& setter);
+
+  std::string GetOptionHint() const override;
+  std::string GetPositionalHint(const std::string& positional) const override;
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override;
+};
+
+class CmdLineAlternativeFlagArgParser : public CmdLineArgParser
+{
+public:
+  using SetterFunction = std::function<void (const std::string &)>;
+
+private:
+  SetterFunction setter;
+  std::string    lastArgumentCalled;
+
+public:
+  explicit CmdLineAlternativeFlagArgParser(SetterFunction&& setter);
+
+  std::string GetOptionHint() const override;
+  std::string GetPositionalHint(const std::string& positional) const override;
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override;
+};
+
+class CmdLineBoolArgParser : public CmdLineArgParser
+{
+public:
+  using SetterFunction = std::function<void (const bool &)>;
+
+private:
+  SetterFunction setter;
+
+public:
+  explicit CmdLineBoolArgParser(SetterFunction&& setter);
+
+  std::string GetOptionHint() const override;
+  std::string GetPositionalHint(const std::string& positional) const override;
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override;
+};
+
+class CmdLineStringArgParser : public CmdLineArgParser
+{
+public:
+  using SetterFunction = std::function<void (const std::string &)>;
+
+private:
+  SetterFunction setter;
+
+public:
+  explicit CmdLineStringArgParser(SetterFunction&& setter);
+
+  std::string GetOptionHint() const override;
+  std::string GetPositionalHint(const std::string& positional) const override;
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override;
+};
+
+class CmdLineStringListArgParser : public CmdLineArgParser
+{
+public:
+  using AppendFunction = std::function<void (const std::string &)>;
+
+private:
+  AppendFunction appender;
+
+public:
+  explicit CmdLineStringListArgParser(AppendFunction&& appender);
+
+  std::string GetOptionHint() const override;
+  std::string GetPositionalHint(const std::string& positional) const override;
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override;
+};
+
+template<typename N>
+class CmdLineNumberArgParser : public CmdLineArgParser
+{
+public:
+  using SetterFunction = std::function<void (const N &)>;
+
+private:
+  SetterFunction setter;
+
+public:
+  explicit CmdLineNumberArgParser(SetterFunction&& setter)
+    : setter(setter)
+  {
+    // no code
+  }
+
+  std::string GetOptionHint() const override
+  {
+    return "number";
+  }
+
+  std::string GetPositionalHint(const std::string& positional) const override
+  {
+    return positional;
+  }
+
+  CmdLineParseResult Parse(CmdLineScanner& scanner) override
+  {
+    if (!scanner.HasNextArg()) {
+      return CmdLineParseResult("Missing value for number argument '"+GetArgumentName()+"'");
+    }
+
+    std::string valueString=scanner.Advance();
+    N           value;
+
+    if (StringToNumber(valueString,value)) {
+      setter(value);
+      return CmdLineParseResult();
+    }
+
+    return CmdLineParseResult("Value for number argument '"+GetArgumentName()+"' is not a valid number '"+valueString+"'");
+  }
+};
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineFlag(Args&& ...args)
+{
+  return std::make_shared<CmdLineFlagArgParser>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineAlternativeFlag(Args&& ...args)
+{
+  return std::make_shared<CmdLineAlternativeFlagArgParser>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineBoolOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineBoolArgParser>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineStringOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineStringArgParser>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineStringListOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineStringListArgParser>(std::forward<Args>(args)...);
+}
+
+template<class T, class ...Args>
+CmdLineArgParserRef CmdLineNumberOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<T>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineIntOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<int>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineUIntOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<unsigned int>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineLongOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<long>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineULongOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<unsigned long>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineSizeTOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<size_t>>(std::forward<Args>(args)...);
+}
+
+template<class ...Args>
+CmdLineArgParserRef CmdLineDoubleOption(Args&& ...args)
+{
+  return std::make_shared<CmdLineNumberArgParser<double>>(std::forward<Args>(args)...);
+}
+
+class CmdLineParser
+{
+private:
+  struct CmdLineOption
+  {
+    CmdLineArgParserRef parser;
+    std::string         option;
+    std::string         argument;
+    bool                stopParsing;
+
+    CmdLineOption(const CmdLineArgParserRef& parser,
+                  const std::string& option,
+                  const std::string& argument,
+                  bool stopParsing)
+    : parser(parser),
+      option(option),
+      argument(argument),
+      stopParsing(stopParsing)
+    {
+      // no code
+    }
+  };
+
+  struct CmdLinePositional
+  {
+    CmdLineArgParserRef parser;
+    std::string         positional;
+
+    CmdLinePositional(const CmdLineArgParserRef& parser,
+                      const std::string& positional)
+      : parser(parser),
+        positional(positional)
+    {
+      // no code
+    }
+  };
+
+  struct CmdLineArgHelp
+  {
+    std::vector<std::string> argTemplates;
+    std::string              helpString;
+
+    CmdLineArgHelp(const std::string& argTemplate,
+                   const std::string& helpString)
+    : helpString(helpString)
+    {
+      argTemplates.push_back(argTemplate);
+    }
+
+    CmdLineArgHelp(const std::vector<std::string>& argTemplates,
+                   const std::string& helpString)
+    : argTemplates(argTemplates),
+      helpString(helpString)
+    {
+      // no code
+    }
+  };
+
+private:
+  std::string    appName;
+  CmdLineScanner scanner;
+
+  std::map<std::string,CmdLineOption> options;
+  std::list<CmdLinePositional>        positionals;
+  std::list<CmdLineArgHelp>           optionHelps;
+  std::list<CmdLineArgHelp>           positionalHelps;
+
+public:
+  CmdLineParser(const std::string& appName,
+                int argc, char* argv[]); // NOLINT
+  CmdLineParser(const std::string& appName,
+                const std::vector<std::string>& arguments);
+
+  void AddOption(const CmdLineArgParserRef& parser,
+                 const std::string& optionName,
+                 const std::string& helpString,
+                 bool stopParsing=false);
+
+  void AddOption(const CmdLineArgParserRef& parser,
+                 const std::vector<std::string>& optionNames,
+                 const std::string& helpString,
+                 bool stopParsing=false);
+
+  void AddPositional(const CmdLineArgParserRef& parser,
+                     const std::string& helpString,
+                     const std::string& argumentName);
+
+  CmdLineParseResult Parse();
+
+  std::string GetHelp(size_t indent=2) const;
+};
+
+extern bool ParseBoolArgument(int argc,
+                              char* argv[], // NOLINT
+                              int& currentIndex,
+                              bool& value);
+
+extern bool ParseStringArgument(int argc,
+                                char* argv[], // NOLINT
+                                int& currentIndex,
+                                std::string& value);
+
+extern bool ParseSizeTArgument(int argc,
+                               char* argv[], // NOLINT
+                               int& currentIndex,
+                               size_t& value);
+extern bool ParseUInt32Argument(int argc,
+                                char* argv[], // NOLINT
+                                int& currentIndex,
+                                uint32_t& value);
+
+
+#endif
---
+++ utils/String.h
@@ -0,0 +1,275 @@
+#ifndef STRING_H
+#define STRING_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2009  Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <limits>
+#include <list>
+#include <memory>
+#include <string>
+#include <chrono>
+#include <cassert>
+
+
+/**
+ * \ingroup Util
+ * Convert the given string to a boolean value
+ *
+ * @param string
+ *    string with a potential boolean value (either 'true' or 'false')
+ * @param value
+ *    value to copy the result to if no error occured
+ * @return
+ *    'true' if the value was parsed, else 'false'
+ */
+extern bool StringToBool(const char* string, bool& value);
+
+/**
+ * Returns a string representation of the given boolean value (either 'true' or 'false')
+ *
+ * @param value
+ *    value to return
+ * @return
+ *    result of the conversion
+ */
+extern const char* BoolToString(bool value);
+
+/**
+ * \ingroup Util
+ * Returns the numerical value of the given character, if the character
+ * is a digit in a numerical value. The current code allows digits
+ * in the range from 0-9 and a-f and A-F. And thus supports
+ * numerical bases from 1-16.
+ */
+extern bool GetDigitValue(char digit,
+                          size_t& result);
+
+template<typename N>
+size_t NumberDigits(const N& number,
+                    size_t base=10)
+{
+  N      value(number);
+  size_t res=0;
+
+  if (std::numeric_limits<N>::is_signed) {
+    if (value<0) {
+      res++;
+    }
+  }
+
+  while (value!=0) {
+    res++;
+    value=value/(N)base;
+  }
+
+  return res;
+}
+
+template<typename N>
+bool StringToNumberSigned(const std::string& string,
+                          N& number,
+                          size_t base=10)
+{
+  assert(base<=16);
+
+  std::string::size_type pos=0;
+  bool                   minus=false;
+
+  number=0;
+
+  if (string.empty()) {
+    return false;
+  }
+
+  /*
+    Special handling for the first symbol/digit (could be negative)
+    */
+  if (base==10 && string[0]=='-') {
+    minus=true;
+    pos=1;
+  }
+  else {
+    size_t digitValue;
+
+    if (!GetDigitValue(string[pos],
+                       digitValue)) {
+      return false;
+    }
+
+    if (digitValue>=base) {
+      return false;
+    }
+
+    /*
+      For signed values with base!=10 we assume a negative value
+    */
+    if (digitValue==base-1 &&
+        string.length()==NumberDigits(std::numeric_limits<N>::max())) {
+      minus=true;
+      number=(N)(base/2);
+    }
+    else {
+      number=(N)digitValue;
+    }
+
+    pos=1;
+  }
+
+  while (pos<string.length()) {
+    size_t digitValue;
+
+    if (!GetDigitValue(string[pos],digitValue)) {
+      return false;
+    }
+
+    if (digitValue>=base) {
+      return false;
+    }
+
+    if (std::numeric_limits<N>::max()/(N)base-(N)digitValue<number) {
+      return false;
+    }
+
+    number=(N)(number*base+digitValue);
+
+    pos++;
+  }
+
+  if (minus) {
+    number=-number;
+  }
+
+  return true;
+}
+
+template<typename N>
+bool StringToNumberUnsigned(const std::string& string,
+                            N& number,
+                            size_t base=10)
+{
+  assert(base<=16);
+
+  std::string::size_type pos=0;
+
+  number=0;
+
+  if (string.empty()) {
+    return false;
+  }
+
+  if (string[0]=='-') {
+    return false;
+  }
+
+  /*
+    Special handling for the first symbol/digit (could be negative)
+    */
+  size_t digitValue;
+
+  if (!GetDigitValue(string[pos],digitValue)) {
+    return false;
+  }
+
+  if (digitValue>=base) {
+    return false;
+  }
+
+  number=(N)digitValue;
+
+  pos=1;
+
+  while (pos<string.length()) {
+    size_t digitValue;
+
+    if (!GetDigitValue(string[pos],
+                       digitValue)) {
+      return false;
+    }
+
+    if (digitValue>=base) {
+      return false;
+    }
+
+    if (std::numeric_limits<N>::max()/(N)base-(N)digitValue<number) {
+      return false;
+    }
+
+    number=(N)(number*base+digitValue);
+
+    pos++;
+  }
+
+  return true;
+}
+
+template<bool is_signed, typename N>
+struct StringToNumberTemplated
+{
+};
+
+template<typename N>
+struct StringToNumberTemplated<true, N>
+{
+  static inline bool f(const std::string& string,
+                       N& number,
+                       size_t base=10)
+  {
+    return StringToNumberSigned<N>(string,number,base);
+  }
+};
+
+template<typename N>
+struct StringToNumberTemplated<false, N>
+{
+  static inline bool f(const std::string& string,
+                       N& number,
+                       size_t base=10)
+  {
+    return StringToNumberUnsigned<N>(string,number,base);
+  }
+};
+
+/**
+ * \ingroup Util
+ * Converts a string holding a (possibly negative) numerical
+ * value of the given base to the numerical value itself.
+ *
+ * Example:
+ *  "-13" => -13
+ */
+template<typename N>
+inline bool StringToNumber(const std::string& string,
+                           N& number,
+                           size_t base=10)
+{
+  return StringToNumberTemplated<std::numeric_limits<N>::is_signed, N>
+  ::f(string,number,base);
+}
+
+/**
+ * \ingroup Util
+ */
+extern bool StringToNumber(const char* string, double& value);
+
+/**
+ * \ingroup Util
+ */
+extern bool StringToNumber(const std::string& string, double& value);
+#endif
