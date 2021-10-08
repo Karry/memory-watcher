5e9bb6f98e05ce84a093dc9d1bdb86b46b7aa6b6
Lukáš Karas <lukas.karas@avast.com>
2021-10-08 09:35:20 +0200

    add CmdLineParsing from libosmscout project

---
+++ utils/String.cpp
@@ -0,0 +1,136 @@
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2009  Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include "String.h"
+
+#include <algorithm>
+#include <cstring>
+#include <sstream>
+#include <thread>
+
+bool GetDigitValue(char digit, size_t& result)
+{
+  switch (digit) {
+    case '1':
+      result=1;
+      return true;
+    case '2':
+      result=2;
+      return true;
+    case '3':
+      result=3;
+      return true;
+    case '4':
+      result=4;
+      return true;
+    case '5':
+      result=5;
+      return true;
+    case '6':
+      result=6;
+      return true;
+    case '7':
+      result=7;
+      return true;
+    case '8':
+      result=8;
+      return true;
+    case '9':
+      result=9;
+      return true;
+    case '0':
+      result=0;
+      return true;
+    case 'a':
+    case 'A':
+      result=10;
+      return true;
+    case 'b':
+    case 'B':
+      result=11;
+      return true;
+    case 'c':
+    case 'C':
+      result=12;
+      return true;
+    case 'd':
+    case 'D':
+      result=13;
+      return true;
+    case 'e':
+    case 'E':
+      result=14;
+      return true;
+    case 'f':
+    case 'F':
+      result=15;
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool StringToBool(const char* string, bool& value)
+{
+  if (std::strcmp(string,"true")==0) {
+    value=true;
+
+    return true;
+  }
+  else if (std::strcmp(string,"false")==0) {
+    value=false;
+
+    return true;
+  }
+
+  return false;
+}
+
+const char* BoolToString(bool value)
+{
+  if (value) {
+    return "true";
+  }
+
+  return "false";
+}
+
+bool StringToNumber(const char* string, double& value)
+{
+  std::istringstream stream(string);
+
+  stream.imbue(std::locale("C"));
+
+  stream >> value;
+
+  return stream.eof();
+}
+
+bool StringToNumber(const std::string& string, double& value)
+{
+  std::istringstream stream(string);
+
+  stream.imbue(std::locale("C"));
+
+  stream >> value;
+
+  return stream.eof();
+}
+
+
