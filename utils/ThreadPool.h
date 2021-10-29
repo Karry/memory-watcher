0e102d1068faadc7cb91237460efb32d9f8e475f
Lukáš Karas <lukas.karas@centrum.cz>
2021-10-29 08:59:06 +0200

    default ThreadPool constructor/desctructor

--- utils/ThreadPool.h
+++ utils/ThreadPool.h
@@ -37,8 +37,8 @@ public slots:
   void checkClosed();
 
 public:
-  ThreadPool();
-  ~ThreadPool();
+  ThreadPool() = default;
+  ~ThreadPool() = default;
 
   QThread *makeThread(QString name);
   void close();
