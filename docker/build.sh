06435be6b27c0ca02ef91f11ae53fa75a6c010ff
Lukáš Karas <lukas.karas@centrum.cz>
2021-11-05 21:32:57 +0100

    add docker builds with clang

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
 SRCDIR="$(readlink -f $(dirname $0)/..)"
-docker run --rm=true -v $SRCDIR:/sources -it memory-watcher-ubuntu-2004
-docker run --rm=true -v $SRCDIR:/sources -it memory-watcher-ubuntu-2104
+docker run --rm=true -v $SRCDIR:/sources -e CXX=g++-10  -e CC=gcc-10 -e BUILD_DIR=build-ubuntu2004-gcc   -it memory-watcher-ubuntu-2004
+docker run --rm=true -v $SRCDIR:/sources -e CXX=clang++ -e CC=clang  -e BUILD_DIR=build-ubuntu2004-clang -it memory-watcher-ubuntu-2004
+docker run --rm=true -v $SRCDIR:/sources -e CXX=g++-11  -e CC=gcc-11 -e BUILD_DIR=build-ubuntu2104-gcc   -it memory-watcher-ubuntu-2104
+docker run --rm=true -v $SRCDIR:/sources -e CXX=clang++ -e CC=clang  -e BUILD_DIR=build-ubuntu2104-clang -it memory-watcher-ubuntu-2104
