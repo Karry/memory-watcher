ea1404882e7fa4a4c3043215c2e0814e679fdc7c
Lukáš Karas <lukas.karas@centrum.cz>
2021-11-05 21:32:57 +0100

    add docker builds with clang

--- docker/ubuntu2004/build.sh
+++ docker/ubuntu2004/build.sh
@@ -4,8 +4,10 @@ if [ ! -d /sources ] ; then
   git clone --recursive git@github.com:Karry/memory-watcher.git /sources
 fi
 
-mkdir -p /sources/build-ubuntu2004
-cd /sources/build-ubuntu2004
+BUILD_DIR=${BUILD_DIR:-build-ubuntu2004}
+
+mkdir -p /sources/$BUILD_DIR
+cd /sources/$BUILD_DIR
 cmake -DCMAKE_UNITY_BUILD=ON -Wno-dev -G "Ninja" ..
 cmake --build . -- all unittests
 ctest -j $(nproc) --output-on-failure
--- docker/ubuntu2104/build.sh
+++ docker/ubuntu2104/build.sh
@@ -4,8 +4,10 @@ if [ ! -d /sources ] ; then
   git clone --recursive git@github.com:Karry/memory-watcher.git /sources
 fi
 
-mkdir -p /sources/build-ubuntu2104
-cd /sources/build-ubuntu2104
+BUILD_DIR=${BUILD_DIR:-build-ubuntu2104}
+
+mkdir -p /sources/$BUILD_DIR
+cd /sources/$BUILD_DIR
 cmake -DCMAKE_UNITY_BUILD=ON -Wno-dev -G "Ninja" ..
 cmake --build . -- all unittests
 ctest -j $(nproc) --output-on-failure
